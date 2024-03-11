9ee1aa0aed80a6cb2ffa63e06933fb9712b23a72
Noah <106498918+swifty-hh@users.noreply.github.com>
2024-03-11 14:52:50 +0900

    Update calc.py

---
+++ calc.py
@@ -0,0 +1,8 @@
+def subtract_numbers():
+    num1 = float(input("뺄셈 할 첫 번째 숫자를 입력하세요: "))
+    num2 = float(input("뺄셈 할 두 번째 숫자를 입력하세요: "))
+
+    result = num1 - num2
+    print("뺄셈 결과: ", result)
+
+subtract_numbers()
