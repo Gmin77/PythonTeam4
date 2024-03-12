accd55720df1f1e12e67b599f9cbf3bd122dd156
Stock05 <qace756@gmail.com>
2024-03-12 11:48:07 +0900

    Revert "text"
    This reverts commit 459c522cf4460e10b34df3d3a814a81f62f97be5.

--- calc.py
+++ calc.py
@@ -1,4 +1,19 @@
-#재헌
+#<<<<<<< HEAD
 def plus(a,b):
     return a+b
+
+def divide(a,b):
+    return a/b 
     
+#>>>>>>> gyeongmin
+
+def subtract_numbers():
+    num1 = float(input("뺄셈 할 첫 번째 숫자를 입력하세요: "))
+    num2 = float(input("뺄셈 할 두 번째 숫자를 입력하세요: "))
+
+    result = num1 - num2
+    print("뺄셈 결과: ", result)
+
+subtract_numbers()
+#>>>>>>> hanjun
+
