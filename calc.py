f4463ab007e131c82765fd835154e91473e21d5f
Stock05 <qace756@gmail.com>
2024-03-12 11:52:53 +0900

    add multi

--- calc.py
+++ calc.py
@@ -17,3 +17,10 @@ def subtract_numbers():
 subtract_numbers()
 #>>>>>>> hanjun
 
+num1 = int(input("첫번째 숫자 : "))
+num2 = int(input("두번째 숫자 : "))
+
+def multiply(x, y):
+    return x * y
+
+print(num1,"*",num2,"=", multiply(num1,num2))
